189b2d81b70d169a41f34a49ce0bcc6ea84fb95e
DBVeit <43188547+DBVeit@users.noreply.github.com>
2020-12-29 16:47:59 -0300

    Login

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include_once("php/conexao.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Contabil</title>
+		<script type="text/javascript" src="jquery/jquery.js"></script>
+		<script type="text/javascript" src="js/includes/index_includes.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/index_style.css">
+	</head>
+	<body class="bodyBackground">
+		<div id="header"></div>
+		<div id="body" align="center"></div>
+		<div id="footer"></div>
+	</body>
+</html>
